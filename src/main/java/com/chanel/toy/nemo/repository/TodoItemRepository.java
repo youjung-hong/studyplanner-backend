5a5736cd0f729a7f22acf87ee7646bf1d47c9e40
youjung hong <youjunghong@macui-MacBook-Pro.local>
2020-01-11 15:34:06 +0900

    Revert "feat: create TodoItem, TodoItemActionLog (model, repository, jpa, h2)"
    This reverts commit ba2d3ccc64edb6ca8b8385f4402f5c1bbf526657.

--- src/main/java/com/chanel/toy/nemo/repository/TodoItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chanel.toy.nemo.repository;
-
-import com.chanel.toy.nemo.model.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
-}
